076f87e3a86bd38345ad4bf1fa290ac8f6fec250
shiwei.gui@gmail.com <GnH@0829>
2018-12-04 16:20:21 +0800

    commited

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * @project: demo
+ * @auther: shiwei
+ * @date: 2018/12/4 16:19
+ * @description: TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
