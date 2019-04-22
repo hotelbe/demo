4b505082b0394ff7781720f49c9413f4621e058f
shiwei gui <shiwei.gui@qq.com>
2019-04-23 00:19:19 +0800

    update master branch...

--- src/main/java/com/example/demo/HelloTest.java
+++ src/main/java/com/example/demo/HelloTest.java
@@ -11,6 +11,7 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("this is a test file");
+        System.out.println("master branch add some codes");
         System.out.println("testing for the git operation");
     }
 }
