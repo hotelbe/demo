2a10f173bfcec798253138568b792a6fc2238b11
shiwei gui <shiwei.gui@qq.com>
2019-04-23 00:06:11 +0800

    commit HelloTest.java

---
+++ src/main/java/com/example/demo/HelloTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/**
+ * @project: demo
+ * @auther: shiwei
+ * @date: 2019/4/23 00:05
+ * @description: TODO
+ */
+public class HelloTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
