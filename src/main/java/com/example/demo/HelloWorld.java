938f863eb8afd89a856ab704bbda44ef1b25dcea
shiwei gui <shiwei.gui@qq.com>
2019-04-23 09:43:00 +0800

    commit 1

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -18,6 +18,8 @@ public class HelloWorld {
         list.add("d");
         list.add("e");
 
+        list.add("f");
+
         for (int i = 0; i < list.size(); i++) {
             System.out.println("i: " + list.get(i));
         }
