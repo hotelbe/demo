43fd22a9ee0fd2a632c504ef4951d2166b9a8bac
shiwei gui <shiwei.gui@qq.com>
2019-04-23 09:26:47 +0800

    commit helloworld

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/**
+ * @project: demo
+ * @auther: shiwei
+ * @date: 2019/4/23 09:25
+ * @description: TODO
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hello");
+        }
+    }
+}
